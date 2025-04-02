82969f92fb4eb010acdd0b534d5e5eb6f099c0a9
thaliaADS <thalia.guedes17@gmail.com>
2025-04-02 17:25:29 -0300

    feat: add before create SalesOrderHeaders logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,4 +1,4 @@
-import { Service } from '@sap/cds';
+import cds, { Service, Request } from '@sap/cds';
 import { Customers } from '@models/sales';
 
 export default (srv: Service) => {
@@ -8,5 +8,19 @@ export default (srv: Service) => {
                 customer.email = `${customer.email}@gmail.com`;
             }
         });
+    });
+    srv.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        if(!params.customer_id) {
+            return request.reject(400, 'Customer inválido');
+        }
+        if(!params.items || params.items?.length === 0 ) {
+            return request.reject(400, 'Itens inválidos');
+        }
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if(!customer) {
+            return request.reject(404, 'Customer não encontrado');
+        }
     });
 }
