825e89921e318384ae161d5c0de5a7f524a06dac
thaliaADS <thalia.guedes17@gmail.com>
2025-04-09 17:30:40 -0300

    refactor: mode before create logic to its own files

--- srv/main.ts
+++ srv/main.ts
@@ -1,6 +1,7 @@
 import cds, { Service, Request } from '@sap/cds';
-import { Customers, Product, Products, SalesOrderHeader, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
+import { Customers, Product, Products, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
 import { customerController } from './factories/controllers/customer';
+import { salesOrderHeaderController } from './factories/controllers/sales-order-header';
 import { FullResquetParams } from './routes/protocols';
 export default (srv: Service) => {
     srv.before('READ', '*', (request: Request) => {
@@ -17,41 +18,11 @@ export default (srv: Service) => {
        (request as unknown as FullResquetParams<Customers>).results = customerController.afterRead(customersList);
     });
     srv.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
-        const params = request.data;
-        const items: SalesOrderItems = params.items;
-        if(!params.customer_id) {
-            return request.reject(400, 'Customer inválido');
+        const result =  await salesOrderHeaderController.beforeCreate(request.data);
+        if(result.hasErrors) {
+            return request.reject(400, result.error?.message as string);
         }
-        if(!params.items || params.items?.length === 0 ) {
-            return request.reject(400, 'Itens inválidos');
-        }
-        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
-        const customer = await cds.run(customerQuery);
-        if(!customer) {
-            return request.reject(404, 'Customer não encontrado');
-        }
-        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
-        const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
-        const products: Products = await cds.run(productsQuery);
-        for(const item of items) {
-            const dbProduct = products.find(product => product.id === item.product_id);
-            if(!dbProduct) {
-            return request.reject(404, `Produto ${item.product_id} não encontrado`);
-            }
-            if(dbProduct.stock === 0) {
-                return request.reject(400, `Produto ${dbProduct.name}(${dbProduct.id}) sem estoque disponível`);
-            }
-        }
-        let totalAmount = 0;
-        items.forEach(item => {
-            totalAmount += (item.price as number) * (item.quantity as number);
-        })
-
-        if(totalAmount > 3000) {
-            const discount = totalAmount * 0.1;
-            totalAmount = totalAmount - discount;
-        }
-        request.data.totalAmount = totalAmount;
+        request.data.totalAmount = result.totalAmount;
     });
     srv.after('CREATE', 'SalesOrderHeaders', async (results: SalesOrderHeaders, request: Request) => {
         const headersAsArray = Array.isArray(results) ? results : [results] as SalesOrderHeaders;
