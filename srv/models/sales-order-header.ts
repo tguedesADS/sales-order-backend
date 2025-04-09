aca18aef9a52071d16128061e467f933fe871135
thaliaADS <thalia.guedes17@gmail.com>
2025-04-09 17:25:10 -0300

    feat: add SalesOrderHeaderModel

---
+++ srv/models/sales-order-header.ts
@@ -0,0 +1,120 @@
+import { SalesOrderItemModel } from "./sales-order-item";
+
+type SalesOrderHeaderProps = {
+    id: string;
+    customerId: string;
+    totalAmount: number;
+    items: SalesOrderItemModel[];
+}
+
+type SalesOrderHeaderPropsWithTotalAmount = Omit<SalesOrderHeaderProps, 'id' | 'totalAmount'>;
+
+type CreationPayload = {
+    customer_id: SalesOrderHeaderProps['customerId'];
+}
+
+type CreationPayloadValidationResult = {
+    hasErrors: boolean;
+    error?: Error;
+}
+
+export class SalesOrderHeaderModel {
+    constructor(private props: SalesOrderHeaderProps) {}
+    
+    public static create(props: SalesOrderHeaderPropsWithTotalAmount): SalesOrderHeaderModel {
+        return new SalesOrderHeaderModel({
+            ...props,
+            id: crypto.randomUUID(),
+            totalAmount: 0
+        });
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get customerId() {
+        return this.props.customerId;
+    }
+
+    public get totalAmount() {
+        return this.props.totalAmount;
+    }
+
+    public get items() {
+        return this.props.items;
+    }
+
+    public set totalAmount(amount: number) {
+        this.totalAmount = amount
+    }
+
+    public validateCreationPayload(params: CreationPayload): CreationPayloadValidationResult {
+        const customerValidationResult = this.validateCustomerOnCreation(params.customer_id);
+        if(customerValidationResult.hasErrors) {
+            return customerValidationResult;
+        }
+        const itemsValidationResult = this.validateItemsOnCreation(this.items);
+        if(itemsValidationResult.hasErrors) {
+            return itemsValidationResult;
+        }
+        return {
+            hasErrors: false
+        };
+    }
+    
+    private validateCustomerOnCreation(customerId: CreationPayload['customer_id']): CreationPayloadValidationResult {
+        if(!customerId) {
+            return {
+                hasErrors: true,
+                error: new Error('Customer inválido')
+            };
+        }
+        return {
+            hasErrors: false
+        };
+    }
+
+    private validateItemsOnCreation(items: SalesOrderHeaderProps['items']): CreationPayloadValidationResult {
+        if(!items || items?.length === 0 ) {
+            return {
+                hasErrors: true,
+                error: new Error('Itens inválidos')
+            };
+        }
+        const itemsErros: string[] = [];
+        items.forEach(item => {
+            const validationResult = item.validateCreationPayload({ product_id: item.productId });
+            if (validationResult.hasErrors) {
+                itemsErros.push(validationResult.error?.message as string);
+            }
+        });
+        if(itemsErros.length > 0) {
+            const messages = itemsErros.join('\n - ');
+            return {
+                hasErrors: true,
+                error: new Error(messages)
+            };
+        }
+        return {
+            hasErrors: false
+        };
+    }
+
+    public calculateTotalAmount(): number {
+        let totalAmount = 0;
+        this.items.forEach(item => {
+            totalAmount += (item.price as number) * (item.quantity as number);
+        })
+        return totalAmount;
+    }
+
+    public calculateDiscount(): number {
+        let totalAmount = this.calculateTotalAmount();
+        if(totalAmount > 3000) {
+            const discount = totalAmount * 0.1;
+            totalAmount = totalAmount - discount;
+        }
+        return totalAmount;
+    }
+}
