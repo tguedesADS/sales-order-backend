fcf1e032de3baaab4c9bc2cbd3a44b05d51a2ef1
thaliaADS <thalia.guedes17@gmail.com>
2025-04-09 17:27:04 -0300

    feat: add CustomerRepository and CustomerRepositoryImpl

---
+++ srv/repositories/customer/protocols.ts
@@ -0,0 +1,6 @@
+import { CustomerModel, CustomerProps } from "srv/models/customer";
+
+export interface CustomerRepository {
+    findById(id: CustomerProps['id']): Promise<CustomerModel | null>;
+
+}
