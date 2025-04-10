c4f1acf9ba4f958b118b6860479938a23f5f6c30
thaliaADS <thalia.guedes17@gmail.com>
2025-04-10 15:52:59 -0300

    feat: add eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,48 @@
-import cds from '@sap/cds/eslint.config.mjs'
-export default [ ...cds.recommended ]
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    {
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['./gen/*.{js,ts}'],
+        files: ['**/*.{js,mjs,ts}'],
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^ignore',
+                    ignoreRestSiblings: true
+                }
+            ],
+            'eol-last': 'error',
+            indent: [
+                'error',
+                4,
+                {
+                    SwitchCase: 1
+                }
+            ],
+            'max-len': ['error', 120],
+            'max-lines-per-function': ['error', 30],
+            'object-curly-spacing': ['error', 'always'],
+            quotes: ['error', 'single'],
+            'quote-props': ['error', 'as-needed'],
+            semi: ['error', 'always'],
+            'sort-imports': [
+                'error', 
+                {
+                    memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                    allowSeparatedGroups: true
+                }
+            ]
+        }
+    }
+]
