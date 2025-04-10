73650a7e4d7b3f5b632d14e39661d19ed779891f
thaliaADS <thalia.guedes17@gmail.com>
2025-04-10 15:12:24 -0300

    feat: add new dependecy for salesOrderHeaderService factory

--- srv/factories/services/sales-order-header.ts
+++ srv/factories/services/sales-order-header.ts
@@ -1,13 +1,16 @@
 import { CustomerRepositoryImpl } from "srv/repositories/customer/implementation";
 import { ProductRepositoryImpl } from "srv/repositories/product/implementation";
+import { SalesOrderLogRepositoryImpl } from "srv/repositories/sales-order-log/implementation";
 import { SalesOrderHeaderServiceImpl } from "srv/services/sales-order-header/implementation";
 import { SalesOrderHeaderService } from "srv/services/sales-order-header/protocols";
 
 export const makeSalesOrderHeaderService = (): SalesOrderHeaderService => {
     const customerRepository = new CustomerRepositoryImpl();
+    const salesOrderLogRepository = new SalesOrderLogRepositoryImpl();
     const productRepository = new ProductRepositoryImpl();
     return new SalesOrderHeaderServiceImpl(
         customerRepository,
+        salesOrderLogRepository,
         productRepository
     );
 }
