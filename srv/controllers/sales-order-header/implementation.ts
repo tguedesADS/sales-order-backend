67ac10745f2818ec97cdb38a804fafdf85a665c6
thaliaADS <thalia.guedes17@gmail.com>
2025-04-10 15:11:11 -0300

    feat: add afterCreate method in SalesOrderHeaderControllerImpl and SalesOrderHeaderController

--- srv/controllers/sales-order-header/implementation.ts
+++ srv/controllers/sales-order-header/implementation.ts
@@ -1,13 +1,17 @@
+import { User } from "@sap/cds";
+
 import { SalesOrderHeaderService } from "srv/services/sales-order-header/protocols";
 import { CreationPayloadValidationResult, SalesOrderHeaderController } from "./protocols";
-import { SalesOrderHeader } from "@models/sales";
+import { SalesOrderHeader, SalesOrderHeaders } from "@models/sales";
 
 export class SalesOrderHeaderControllerImpl implements SalesOrderHeaderController {
-    constructor(
-        private readonly service: SalesOrderHeaderService
-    ) {}
+    constructor(private readonly service: SalesOrderHeaderService) {}
 
     public async beforeCreate(params: SalesOrderHeader): Promise<CreationPayloadValidationResult> {
         return this.service.beforeCreate(params);
+    }
+
+    public async afterCreate(params: SalesOrderHeaders, loggedUser: User): Promise<void> {
+        return this.service.afterCreate(params, loggedUser);
     }
 }
