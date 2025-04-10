5ccf334d2271e4e0f7fff2c7650c36483b0c33a8
thaliaADS <thalia.guedes17@gmail.com>
2025-04-10 15:06:30 -0300

    feat: add LoggedUserModel

---
+++ srv/models/logged-user.ts
@@ -0,0 +1,34 @@
+type LoggedUserProps = {
+    id: string;
+    roles: string[];
+    attributes: LoggedUserAttributesProps;
+}
+
+type LoggedUserAttributesProps = {
+    id: number;
+    groups: string[];
+}
+
+export class LoggedUserModel {
+    constructor(private props: LoggedUserProps) {}
+
+    public static create(props: LoggedUserProps) {
+        return new LoggedUserModel(props);
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get roles() {
+        return this.props.roles;
+    }
+
+    public get attributes() {
+        return this.props.attributes;
+    }
+
+    public toStringifiedObject(): string {
+        return JSON.stringify(this.props);
+    }
+}
