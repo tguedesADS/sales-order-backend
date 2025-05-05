ad3b2463fa3428e3aebc76bfbaecb64babc29688
thaliaADS <thalia.guedes17@gmail.com>
2025-05-05 09:31:16 -0300

    refactor: implement monad Either approach in SalesReportServiceImpl and SalesReportService

--- srv/services/sales-report/implementation.ts
+++ srv/services/sales-report/implementation.ts
@@ -1,17 +1,27 @@
+import { Either, left, right } from '@sweet-monads/either';
+
 import { ExpectedResult as SalesReport } from '@models/db/types/SalesReport';
 
+import { AbstractError, NotFoundError, ServerError } from '@/errors';
 import { SalesReportRepository } from '@/repositories/sales-report';
 import { SalesReportService } from '@/services/sales-report';
 
 export class SalesReportServiceImpl implements SalesReportService {
     constructor(private readonly salesReportRepository: SalesReportRepository) {}
 
-    public async findByDays(days = 7): Promise<SalesReport[]> {
-        const reportData = await this.salesReportRepository.findByDays(days);
-        if (!reportData) {
-            return [];
+    public async findByDays(days = 7): Promise<Either<AbstractError, SalesReport[]>> {
+        try {
+            const reportData = await this.salesReportRepository.findByDays(days);
+            if (!reportData) {
+                const stack = new Error().stack as string;
+                return left(new NotFoundError('Nenhum dado encontrado para o período selecionado', stack));
+            }
+            const mappedData = reportData.map((r) => r.toObject());
+            return right(mappedData);
+        } catch (error) {
+            const errorInstance = error as Error;
+            return left(new ServerError(errorInstance.stack as string, errorInstance.message));
         }
-        return reportData.map((r) => r.toObject());
     }
 
     public async findByCustomerId(customerId: string): Promise<SalesReport[]> {
--- srv/services/sales-report/protocols.ts
+++ srv/services/sales-report/protocols.ts
@@ -1,6 +1,9 @@
+import { Either } from '@sweet-monads/either';
+
 import { ExpectedResult as SalesReport } from '@models/db/types/SalesReport';
 
+import { AbstractError } from '@/errors';
 export interface SalesReportService {
-    findByDays(days: number): Promise<SalesReport[]>;
+    findByDays(days: number): Promise<Either<AbstractError, SalesReport[]>>;
     findByCustomerId(customerId: string): Promise<SalesReport[]>;
 }
